5eb25d9a671b761348fcb13200eadba0f3cd474d
jian.li <lijian_820@163.com>
2022-09-20 09:39:08 +0800

    disadvantages of Executors

--- src/main/java/com/my7/pool/ThreadPoolDemo2.java
+++ src/main/java/com/my7/pool/ThreadPoolDemo2.java
@@ -15,6 +15,15 @@ import java.util.concurrent.TimeUnit;
  * BlockingQueue<Runnable> workQueue  阻塞队列
  * ThreadFactory threadFactory  线程工厂
  * RejectedExecutionHandler handler  拒绝策略
+ *
+ *  阿里巴巴开发手册规定: 不允许使用Executors的方式手动创建线程池, 而是要通过ThreadPoolExecutor的方式,
+ *  这样的处理方式让写的同学更加明确线程池的运行规则, 规避资源耗尽的风险
+ *
+ *  Executors返回的线程池对象的弊端如下:
+ *      FixedThreadPool和SingleThreadPool:
+ *          允许的请求队列长度为Integer.MAX_VALUE, 可能会堆积大量的请求, 从而导致OOM(out of memory 内存溢出)
+ *       CachedThreadPool和ScheduledThreadPool:
+ *          允许的创建线程数量为Integer.MAX_VALUE, 可能会创建大量的线程, 从而导致OOM
  * </p>
  *
  * @author jian.li on 2022/9/18 23:28
